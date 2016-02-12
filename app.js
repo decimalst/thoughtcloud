ba24657249f8aa730035b987dbbf29cdb79da9d2
Riley Shaw <rlyshw@gmail.com>
2016-02-12 10:07:22 -0500

    Hello world http server set up

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
+// import the http package as an object.
 var http = require("http");
-//basically everything is of an abstract object type
-// meaning every defined thing has dot other things with it
-// these are called prototypes, I think. Prototypes are just
-// the shit that comes with your object. Like a string has a
-// .size attribute prototyped or some shit. proto-post-type
-// you can override it and get into the nitty gritty details but
-// fuck that. for now, just don't worry about that complex stuff.
-// more details can be found in the eloquent js book.
 
-// Alright javascript is really cool.
-// Super easy to read literally 5 year olds can use this shit
+// making a new server from http.createServer{https://nodejs.org/api/http.html#http_http_createserver_requestlistener}
+// Docs on server object here: https://nodejs.org/api/http.html#http_class_http_server
+var server = http.createServer(function(req,resp){
+  console.log("Pong!")
+  resp.end("hello world! -ping");
+});
 
-//this is a variable definition, you do var varname. it's easy
-// javascript doesn't have types, ``
-var a;
-var b = 1;
-var cuck = "myshitup";
-
-// pretty sure you always use var for any variable name,
-// even for defining functions
-var adder = function(a,b){
-  return a+b;
-}
-
-var server = http.createServer(function())
+console.log("Server is listening on port 300...")
+server.listen(300);
