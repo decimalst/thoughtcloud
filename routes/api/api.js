1d782630d466dd475d7c189512e8bcbeb1bf0e8a
Riley Shaw <rlyshw@gmail.com>
2016-02-14 17:47:18 -0500

    added getting and updating posts

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
-mongoose.connect("localhost");
+mongoose.connect("localhost","thoughts");
 var Post = require('../../models/posts.js');
 
 router.post('/post',(req,res,next)=>{
@@ -9,12 +9,30 @@ router.post('/post',(req,res,next)=>{
     title: req.body.title,
     body: req.body.body,
     tags: req.body.tags
-  },(err, small)=>{
+  },(err, post)=>{
     if(err) return res.status(500).end("post failed");
-    console.log("aight")
-    res.json(small._id);
+    res.json(post._id);
     res.status(200).end("OK");
   });
+})
+
+router.get('/post/:id',(req, res)=>{
+  Post.findById(req.params.id,(err,post)=>{
+    if(err) return res.status(404).end("post not found");
+    res.json(post);
+    res.status(200).end("OK");
+  });
+});
+
+router.post('/post/:id',(req,res)=>{
+  Post.findByIdAndUpdate(req.params.id,
+  {$push: {"tags": {$each:req.body.tags}}},
+  {safe: true, new : true},
+  (err,post)=>{
+    if(err) return res.status(404).end("post not found"+err);
+    res.json(post);
+    res.status(200).end("OK")
+  })
 })
 
 module.exports = router;
