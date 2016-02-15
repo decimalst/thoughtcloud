1446209c23e3ebba737e861df785eea32e82af84
Riley Shaw <rlyshw@gmail.com>
2016-02-14 20:25:23 -0500

    cleaning, commenting

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-// import the http package as an object.
-var http = require("http");
-var app = require('express')();
-var bodyParser = require('body-parser');
-var api = require("./routes/api/api.js")
+// It all starts here
 
+//Using express now. Read about it.
+//http://expressjs.com/en/guide/routing.html
+
+// import the depdencies as objects.
+//express is a framework for http servers in node
+var app = require('express')();
+//bodyParser is a middleware for processing POST requests
+var bodyParser = require('body-parser');
+//API is the control interface router
+var api = require("./routes/api/posts.js")
+
+//requests will pass through bodyParser middleware first
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Defines what happens at the homepage
 app.get('/',function(req,res){
+  //for now we just write Hello into the body.
+  //We can use some rendering engines to do cooler things
   res.send('Hello')
 })
+
+//use our API router
 app.use('/api',api);
 
 console.log("Server is listening on port 300...\nNavigate to localhost:300")
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,46 +1,60 @@
+//this is where the API control flow lives
+
+//router is like a mini-app.
 var router = require('express').Router();
+//mongoose handles js talking to mongodb
 var mongoose = require('mongoose');
+//connect to the db, use collection "thoughts"
 mongoose.connect("localhost","thoughts");
+//We need to bring in Post objects as
+// mongoose document objects,
 var Post = require('../../models/posts.js');
 
+
+//Acutal routing below
+//get posts that match req.query.tags
 router.get('/posts',(req,res)=>{
-  //res.json(req.query);
+
+  //find a query. The matched post will include
+  // all tags defined in req.query.tags
   Post.find({tags:{$all:req.query.tags}},(err,post)=>{
     if(err) return res.status(404).end("no posts found");
-    console.log(req.query.tags,post);
     res.json(post);
     res.status(200).end("OK");
   })
 })
 
+//make a new post
 router.post('/post',(req,res,next)=>{
   // Here we will write to the database.
-  Post.create({
+  Post.create({ //make a new Post from the req.body
     title: req.body.title,
     body: req.body.body,
     tags: req.body.tags
   },(err, post)=>{
     if(err) return res.status(500).end("post failed");
-    res.json(post._id);
+    res.json(post._id);//resp with new post ID
     res.status(200).end("OK");
   });
 })
 
+//find post by ID
 router.get('/post/:id',(req, res)=>{
   Post.findById(req.params.id,(err,post)=>{
     if(err) return res.status(404).end("post not found");
-    res.json(post);
+    res.json(post);//send entire post back
     res.status(200).end("OK");
   });
 });
 
+//add tags to post by ID
 router.post('/post/:id',(req,res)=>{
   Post.findByIdAndUpdate(req.params.id,
-  {$push: {"tags": {$each:req.body.tags}}},
+  {$push: {"tags": {$each:req.body.tags}}},//push tags to array
   {safe: true, new : true},
   (err,post)=>{
     if(err) return res.status(404).end("post not found"+err);
-    res.json(post);
+    res.json(post);//give updated post back
     res.status(200).end("OK")
   })
 });
