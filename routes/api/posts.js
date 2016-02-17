a712df611999b1740b908a664d963a793dce8b3d
Riley Shaw <rlyshw@gmail.com>
2016-02-16 23:36:38 -0500

    Comments for recent work

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -18,39 +18,48 @@ router.get('/posts',(req,res)=>{
   // all tags defined in req.query.tags
   var query = {$or:[]}
   var tags= [].concat(req.query.tags);
-  tags.forEach((tag)=>{
+  tags.forEach((tag)=>{//convert tags array into a mongodb query.
     var obj = {}
     obj['tags.'+tag]={$gt:0};
     query.$or.push(obj);
   })
-  Post.find(query,(err,posts)=>{
+  Post.find(query,(err,posts)=>{//query= {$or:[{tags.*:{$gt:0}}]}
+  // query line means any post who has more than zero tags named tag.* where *
+  // is one of several tag names from the request. Each tag from request is pushed
+  // into the $or array, so each is tested.
     if(err) return res.status(404).end("no posts found");
 
     var relPosts = [];
 
-    for(var i=0;i<posts.length;i++){
-      var post = {post:posts[i], relevancy:0};
-      for(var j=0;j<tags.length;j++){
+    //Here's the relevancy checker
+    for(var i=0;i<posts.length;i++){//check all matched posts
+      var post = {post:posts[i], relevancy:0};//make new object
+      for(var j=0;j<tags.length;j++){//check all the search tags
         var total=0;
-        for(var t in post.post.tags){
+        for(var t in post.post.tags){//sum up total votes
           if(post.post.tags.hasOwnProperty(t))total+=post.post.tags[t];
         }
-        for(var t in post.post.tags){
-          if(t==tags[j]){
+        for(var t in post.post.tags){//compute relevancy for all tags
+          if(t==tags[j]){//only matched tags matter
+            //if relevancy already exists(one search term was already matched)
+            //then increment the relevancy by the next search term relevancy.
             if(post.relevancy>0)post.relevancy+=(post.post.tags[t]/total);
+
+            //otherwise, just set relevancy to the current weight of tag wrt total.
             else post.relevancy=(post.post.tags[t]/total);
           }
         }
       }
+      //add the computed posts to relPosts for export
       relPosts.push(post);
     }
-    console.log(relPosts);
-    relPosts.sort((a,b)=>{
+
+    relPosts.sort((a,b)=>{//sort relPosts by most relevant first
       if(a.relevancy>b.relevancy)return -1;
       if(a.relevancy<b.relevancy)return 1;
       return 0;
     })
-    res.json(relPosts);
+    res.json(relPosts);//Ship it!
     res.status(200).end("OK");
   })
 })
@@ -58,8 +67,9 @@ router.get('/posts',(req,res)=>{
 //make a new post
 router.post('/post',(req,res,next)=>{
   var tags = {};
-  req.body.tags.forEach((tag)=>{
-    tags[tag]=1;
+  req.body.tags.forEach((tag)=>{//make tags into an object for saving into db
+    tags[tag]=1;//tags = {tag1:1,tag2:1,...}
+    //when posts are first created the tags can't be added more than once.
   });
   // Here we will write to the database.
   Post.create({ //make a new Post from the req.body
@@ -75,7 +85,7 @@ router.post('/post',(req,res,next)=>{
 
 //find post by ID
 router.get('/post/:id',(req, res)=>{
-  Post.findById(req.params.id,(err,post)=>{
+  Post.findById(req.params.id,(err,post)=>{//get the post by the id from the url
     if(err) return res.status(404).end("post not found");
     res.json(post);//send entire post back
     res.status(200).end("OK");
@@ -87,12 +97,12 @@ router.post('/post/:id',(req,res)=>{
   Post.findOne({_id:req.params.id},
   (err,post)=>{
     if(err) return res.status(404).end("post not found"+err);
-    req.body.tags.forEach((tag)=>{
-      if(!post.tags[tag])post.tags[tag]=1;
-      else post.tags[tag]++;
+    req.body.tags.forEach((tag)=>{//for each tag
+      if(!post.tags[tag])post.tags[tag]=1;//create if doesn't exist
+      else post.tags[tag]++;//increment if it does exist
     })
-    post.markModified('tags');
-    post.save((err)=>{
+    post.markModified('tags');//since tags is a multitype, we have to mark it for update
+    post.save((err)=>{//save new post.
       if(err) {console.log(err); return res.status(500).end(err);}
       res.json(post);//give updated post back
       res.status(200).end("OK");
--- routes/pub/public.js
+++ routes/pub/public.js
@@ -2,53 +2,61 @@ var router = require('express').Router();
 var request = require('request');
 var querystring = require('querystring');
 
+
+// Handler for url/public/ endpoint
 router.get("/",(req, res)=>{
   res.render("search");
 });
+
+//search request handler
 router.get("/search",(req,res)=>{
   var tags = req.query.tags.split(',');
   var url = 'http://localhost:300/api/posts?';
   tags.forEach((val)=>{
-    url+=('&tags='+val);
+    url+=('&tags='+val);//construct the unique query string for the API to parse
   })
-  request.get(url,
+  request.get(url,//get posts from API
   (err,resp,body)=>{
     if(!err&&resp.statusCode==200){
+      //if we got the posts successfully, print them out. pass tags too
       res.render('posts',{tags:req.query.tags,posts:JSON.parse(body)});
     }
     else{res.status(500).end(err)}
   })
-  //res.json(tags);
 });
+
+//get a specific post
 router.get('/:id',(req,res)=>{
-  var url = 'http://localhost:300/api/post/'+req.params.id;
-  request.get(url,(err,resp,body)=>{
+  var url = 'http://localhost:300/api/post/'+req.params.id;//construct api request
+  request.get(url,(err,resp,body)=>{//send api request
     if(!err&&resp.statusCode==200){
+      //render post
       res.render('post',{post:JSON.parse(body)});
     }
     else{res.status(500).end(err)}
   })
 });
+//add tags to post
 router.post('/:id/tag',(req,res)=>{
-  var url = 'http://localhost:300/api/post/'+req.params.id;
-  var tags = req.body.tags.split(",");
-  request.post(url,{form:{tags:tags}},(err,resp,body)=>{
-    if(!err&&resp.statusCode==200){
-      res.redirect('/public/'+req.params.id);
+  var url = 'http://localhost:300/api/post/'+req.params.id;//construct api route
+  var tags = req.body.tags.split(",");//construct tags array
+  request.post(url,{form:{tags:tags}},(err,resp,body)=>{//fire api request
+    if(!err&&resp.statusCode==200){//if successful
+      res.redirect('/public/'+req.params.id);//reload page
     }
     else{res.status(500).end(err)}
   })
 })
 router.post('/post',(req,res)=>{
-  var url = 'http://localhost:300/api/post'
-  var tags= req.body.tags.split(",");
-  request.post(url,{form:{
+  var url = 'http://localhost:300/api/post'//where to send request
+  var tags= req.body.tags.split(",");//get tags into an array
+  request.post(url,{form:{//construct post form
     title:req.body.title,
     body:req.body.body,
     tags:tags
   }},(err,resp,body)=>{
     if(!err&&resp.statusCode==200){
-      res.redirect('/public/'+JSON.parse(body))
+      res.redirect('/public/'+JSON.parse(body))//go to new post
     }
   })
 })
